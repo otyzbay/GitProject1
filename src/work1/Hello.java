5a90f69adf9e3295f3b47aeb74fca661b694ce0a
Zhazira Otyzbay <jazirakentauly@gmail.com>
2022-12-15 17:48:15 +0300

    added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Work1 started ");
         System.out.println("Created Git Repository");
+        System.out.println("Made few commits");
+
 
     }
 
